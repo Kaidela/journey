01a193f231c1a73ffd39e7d30ea2708ebff865f6
Sarah Stewart <kaidela@gmail.com>
2013-08-15 07:09:17 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Journey::Application.config.secret_key_base = 'af85d3c71d8727940a42ef6b388ba709c5cca91b4c601e9d1964b02f61df25afa8a7eb12435616feba310666c374c2abf34ed23cfbbe66df05801b418c8bc5d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Journey::Application.config.session_store :cookie_store, key: '_journey_session'
