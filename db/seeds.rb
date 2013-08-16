2650e08d920c9c52067f916fcc41a1596abd5a90
Sarah Stewart <kaidela@gmail.com>
2013-08-16 21:56:23 +1000

    Added database seed for day model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+start_date = Date.new(2013,8,30)
+(1...14).each do |day_number|
+  Day.create(:date => start_date + day_number -1, :day_number => day_number)
+end
