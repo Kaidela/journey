15ffcf45760e1c9d5f37b3377eb4e2787cb99ee6
Sarah Stewart <kaidela@gmail.com>
2013-08-17 21:29:56 +1000

    Added in functions and classes for today, tomorrow, yesterday, future, and past.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -9,4 +9,17 @@ module WelcomeHelper
   def day_order(day)
     14 - day.day_number
   end
+  def day_view(day)
+    if day.is_current?
+      'today'
+    elsif day.is_tomorrow?
+      'tomorrow'
+    elsif day.is_yesterday?
+      'yesterday'
+    elsif day.is_future?
+      'future'
+    elsif day.is_past?
+      'past'
+    end
+  end 
 end
--- app/models/day.rb
+++ app/models/day.rb
@@ -3,6 +3,18 @@ class Day < ActiveRecord::Base
     day_number.to_words.humanize
   end
   def is_current?
+    day_number == 3
+  end
+  def is_tomorrow?
+    day_number == 4
+  end
+  def is_yesterday?
+    day_number == 2
+  end
+  def is_future?
+    day_number >= 5
+  end
+  def is_past?
     day_number == 1
   end
 end
