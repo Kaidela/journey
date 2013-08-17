55784cca14978ae476ff278fec566f8dd1acc4d4
Sarah Stewart <kaidela@gmail.com>
2013-08-17 21:11:42 +1000

    Added day state and day order to helper to define view

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,12 @@
 module WelcomeHelper
+  def day_state(day)
+    if day.is_current?
+      'expanded' 
+    else
+      'collapsed'
+    end
+  end
+  def day_order(day)
+    14 - day.day_number
+  end
 end
--- app/models/day.rb
+++ app/models/day.rb
@@ -1,2 +1,8 @@
 class Day < ActiveRecord::Base
+  def human_day_number
+    day_number.to_words.humanize
+  end
+  def is_current?
+    day_number == 1
+  end
 end
