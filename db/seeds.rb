9536ab1ca06917d410ce07ae81df12b15bb1cd09
Sarah Stewart <kaidela@gmail.com>
2013-08-19 07:56:36 +1000

    Fixed location generation bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 START_DATE = Date.new(2013,8,30)
 DESTINATIONS = [
-  {:arrive_date => Date.new(2013,8,29), :leave_date => Date.new(2013,8,30), :location => 'Melbourne'},
+  {:arrive_date => Date.new(2013,8,30), :leave_date => Date.new(2013,8,30), :location => 'Melbourne'},
   {:arrive_date => Date.new(2013,8,31), :leave_date => Date.new(2013,9,5), :location => 'Beijing'},
   {:arrive_date => Date.new(2013,9,6), :leave_date => Date.new(2013,9,8), :location => 'Xi\'an'},
   {:arrive_date => Date.new(2013,9,8), :leave_date => Date.new(2013,9,12), :location => 'Shanghai'},
-  {:arrive_date => Date.new(2013,9,12), :leave_date => Date.new(2013,8,13), :location => 'Beijing'},
+  {:arrive_date => Date.new(2013,9,12), :leave_date => Date.new(2013,9,13), :location => 'Beijing'},
   {:arrive_date => Date.new(2013,9,14), :leave_date => Date.new(2013,9,14), :location => 'Singapore'},
   {:arrive_date => Date.new(2013,9,15), :leave_date => Date.new(2013,9,15), :location => 'Melbourne'},
 ]
@@ -12,7 +12,7 @@ DESTINATIONS = [
 def start_location(day_number)
   day = START_DATE + day_number
   day_destinations = DESTINATIONS.select do |destination|
-    destination[:arrive_date] >= day and destination[:leave_date] <= day
+    day >= destination[:arrive_date] and day <= destination[:leave_date]
   end
   day_destinations.first[:location]
 end
@@ -20,7 +20,7 @@ end
 def end_location(day_number)
   day = START_DATE + day_number
   day_destinations = DESTINATIONS.select do |destination|
-    destination[:arrive_date] >= day and destination[:leave_date] <= day
+    day >= destination[:arrive_date] and day <= destination[:leave_date]
   end
   day_destinations.last[:location]
 end
