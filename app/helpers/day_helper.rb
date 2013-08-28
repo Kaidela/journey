1cdf02124f554dbab23498ed2ebe5d0eb24bfd7b
Sarah <sarah@ubuntu.(none)>
2013-08-28 19:56:23 +1000

    Created helper for day view.

--- app/helpers/day_helper.rb
+++ app/helpers/day_helper.rb
@@ -1,2 +1,15 @@
 module DayHelper
+	def day_detail_class(day)
+		if day.is_current?
+      'today'
+    elsif day.is_tomorrow?
+      'tomorrow'
+    elsif day.is_yesterday?
+      'yesterday'
+    elsif day.is_future?
+      'future'
+    elsif day.is_past?
+      'past'
+    end
+	end
 end
