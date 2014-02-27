95985a4d6974e0fb8c728cb5284cef3cadbf9a85
Sarah Stewart <kaidela@gmail.com>
2014-02-28 07:34:10 +1100

    Got comments posting to day card.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,15 @@
 class CommentsController < ApplicationController
+	wrap_parameters(:comment)
+	
 	def index 
 		@day = Day.find(params[:day_id])
 		@comments = @day.comments
 	end
+	def create
+		@comment = Comment.create(comment_params)
+	end
+	private
+		def comment_params
+			params.require(:comment).permit(:name,:email,:body,:day_id)
+		end
 end
