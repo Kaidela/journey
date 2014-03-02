b4dc289e5578f858e41731ebf4875a34de8fbde5
Sarah Stewart <kaidela@gmail.com>
2014-03-03 07:17:14 +1100

    Fixed up Javascript bug when clicking submit button.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,10 +1,12 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
     $('.day-card').click(function() {
-        $('.day-card').addClass('collapsed');
-        $('.day-card').removeClass('expanded');
-        $(this).addClass('expanded');
-        var cardPosition = $(this).position();
-        $( document ).scrollTop(cardPosition.top);
+    	if (!$(this).hasClass('expanded')) {
+	        $('.day-card').addClass('collapsed');
+	        $('.day-card').removeClass('expanded');
+	        $(this).addClass('expanded');
+	        var cardPosition = $(this).position();
+	        $( document ).scrollTop(cardPosition.top);
+    	}
     });
 });
