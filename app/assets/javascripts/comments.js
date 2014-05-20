d6ad852ee01944c26ae59bd09e4f75fcffd6015d
Ryan Scott <atthealma@gmail.com>
2014-05-21 08:05:09 +1000

    Implemented the comments appearing in a modal.

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,13 +1,16 @@
 $(document).ready(
 	function() {
 		$('.CreateComment').submit(function() {
+			var form = $(this);
+			var comments = form.closest('.day-card-comments');
 			$.post($(this).attr('action'), $(this).serialize(), function(data){
-				$('.day-comments-list').prepend(data);
-				$('#comment_name').val('');
-				$('#comment_email').val('');
-				$('#comment_body').val('');
+				$('#comment_name', form).val('');
+				$('#comment_email', form).val('');
+				$('#comment_body', form).val('');
+
+				$('.day-comments-list', comments).prepend(data);
 			});
 			return false;
-		});	
+		});
 	}
 );
