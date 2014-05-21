d0afd960448fa615788d84e606910767ccf325bc
Sarah Stewart <kaidela@gmail.com>
2014-05-21 22:27:53 +1000

    Updated the header.

--- db/schema.rb
+++ db/schema.rb
@@ -75,10 +75,4 @@ ActiveRecord::Schema.define(version: 20140519220916) do
     t.datetime "updated_at"
   end
 
-  create_table "trips", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
